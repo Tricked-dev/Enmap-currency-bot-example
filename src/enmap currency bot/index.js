94726f562641a43862d3eabe2bc4b276bea15a40
SkyBlockDev <72335827+SkyBlockDev@users.noreply.github.com>
2020-11-30 17:52:27 +0000

    removed some useless code

--- src/enmap currency bot/index.js
+++ src/enmap currency bot/index.js
@@ -1,17 +1,17 @@
-const Discord = require('discord.js');
-const WOKCommands = require('wokcommands');
-const config = require('./config.json');
-const client = new Discord.Client();;
-const Enmap = require("enmap");
+const Discord = require('discord.js'); // requireing djs
+const WOKCommands = require('wokcommands'); // command hand;er
+const config = require('./config.json'); //setting config
+const client = new Discord.Client();; // making a djs client
+const Enmap = require("enmap"); // the almighty enmap
 
-client.on('ready', () => {
+client.on('ready', () => { // enabling WOKcommands
 	new WOKCommands(client, 'commands', 'features')
 		.setMongoPath(config.mongoPath)
-		.setSyntaxError('Incorrect syntax! Please use {PREFIX}{COMMAND} {ARGUMENTS}').setCategoryEmoji('Points', '🤑').setCategoryEmoji('Fun', '🎮').setCategoryEmoji('Moderation', '😎').setCategoryEmoji('Other', '😲').setCategoryEmoji('Hypixel Skyblock', '😲')
+		.setSyntaxError('Incorrect syntax! Please use {PREFIX}{COMMAND} {ARGUMENTS}').setCategoryEmoji('Points', '🤑').setCategoryEmoji('Fun', '🎮')
     });
 
 
-client.userProfiles = new Enmap({
+client.userProfiles = new Enmap({ // making the enman check features oin how data is stored
     name: 'userProfiles',
     fetchAll: true,
     autoFetch: true,
